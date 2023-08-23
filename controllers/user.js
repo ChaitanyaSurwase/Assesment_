ee032a161a970ab66554025910f50512acae17d3
surwase chaitanya <85385356+ChaitanyaSurwase@users.noreply.github.com>
2023-08-23 09:52:50 +0530

    Update user.js

--- controllers/user.js
+++ controllers/user.js
@@ -66,8 +66,43 @@ exports.login = async (req, res) => {
 };
 
 exports.getAllUsers = async (req, res) => {
-  //... (as previously provided, with pagination and sorting)
+  try {
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;  // default page is 1
+    const limit = parseInt(req.query.limit, 10) || 10;  // default limit is 10 items
+    const skip = (page - 1) * limit;
+
+    // Sorting
+    let sort = {};
+    if (req.query.sortBy && ['asc', 'desc'].includes(req.query.order)) {
+      sort[req.query.sortBy] = req.query.order === 'desc' ? -1 : 1;
+    }
+
+    const users = await User.find({}, { password: 0 })  // Excluding passwords
+      .skip(skip)
+      .limit(limit)
+      .sort(sort);
+
+    // Count total documents to calculate total pages
+    const total = await User.countDocuments();
+
+    const totalPages = Math.ceil(total / limit);
+
+    return res.status(200).send({
+      success: true,
+      count: users.length,
+      total,
+      totalPages,
+      page,
+      limit,
+      data: users
+    });
+  } catch (err) {
+    console.log("Error fetching all users:", err);
+    return res.status(500).send({ success: false, message: "Internal server error" });
+  }
 };
+
 
 exports.updateUser = async (req, res) => {
   try {
